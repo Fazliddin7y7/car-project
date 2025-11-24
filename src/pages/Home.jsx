8a7716cc5feae7e6ffa67c3cdda16b5d21782816
Ismingiz <email@example.com>
2025-11-24 16:34:08 +0500

    home pusshing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,11 @@ import Footer from "../components/Footer";
 const HomeContainer = styled.div`
 width: 100%;
 height: 100%;
-background-color: blue;
-
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+background-color: #ffffff;
 `;
 
 
@@ -21,7 +24,9 @@ export default function Home() {
     return (
         <>
         <HomeContainer>
+
             <h1>hello</h1>
+            <h2>asadbek</h2>
 
 
         </HomeContainer>
