688bea1122d3e05aecaf6238f1806b0e7bcc57e5
Ismingiz <email@example.com>
2025-11-24 16:03:20 +0500

    code pusshing first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from "../src/pages/Home"
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Home />
+
+
+    </>
+  )
+}
+
+export default App
